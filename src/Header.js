d2c62ccdec005a2ce3aef01018c518559b50e5ab
JAY2415 <137811465+JAY2415@users.noreply.github.com>
2023-12-24 23:09:42 +0530

    Create Header.js

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+export default function Header() {
+  return (
+    <header>
+    <nav className="nav">
+      <img className="logo" src={logo} alt="logo" />
+      <ul className="nav-items">
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    </header>
+  )
+}
