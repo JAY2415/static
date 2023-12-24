16df6a8f82f8dfea6c8e07f2cc64ffc2d808257c
JAY2415 <137811465+JAY2415@users.noreply.github.com>
2023-12-24 17:13:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+    <nav>
+      <img src={logo} alt="logo" />
+      <ul className="nav-items>
+        <li>Pricing</li>
+        <li>About</li
+        <li>Contact</li>
+      </ul>
+    </nav>
+    </header>
+  )
+}
+function MainContent() {
+  return (
+    
+  )
+}
+
+function Footer() {
+  return (
+    
+  )
+}
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
-  );
+  )
 }
 
 export default App;
