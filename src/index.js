5dc224011368ecb63698a20f86984f0e67c0ff8f
JAY2415 <137811465+JAY2415@users.noreply.github.com>
2023-12-24 23:24:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Header from './Header';
 import Footer from './Footer';
 import MainContent from './MainContent';
-import './App.css'
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
