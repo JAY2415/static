115fae9b46f383377ff6136b11b60a53def5739e
JAY2415 <137811465+JAY2415@users.noreply.github.com>
2023-12-25 11:11:43 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Header() {
     <nav className="nav">
       <img className="logo" src={logo} alt="logo" />
       <h3 className="nav-h3">ReactFacts</h3>
-      <h4 className="nav-h4">by JAY</>
+      <h4 className="nav-h4">by JAY</h4>
     </nav>
     </header>
   )
