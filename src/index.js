75c08d15bc7e8466ed89648973a9ff24782027cd
JAY2415 <137811465+JAY2415@users.noreply.github.com>
2023-12-25 11:09:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Navbar from './components/Navbar';
 import MainContent from './components/MainContent';
 import './App.css';
+import logo from './logo.svg'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
